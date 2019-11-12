c9581e5d09189e7b6a891a6d938572024839d111
zeca <zeca6340zaq1>
2019-11-12 15:53:42 -0300

    Adicionando eco

--- bin/make-env.sh
+++ bin/make-env.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 for s in $( cat ecosystem.json | strip-json-comments | jq -r '.apps[0].env|@text' | jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' );
-    do echo $s >> .env.teste;
+    do echo $s >> .env;
 done;
