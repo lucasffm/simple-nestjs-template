713bd7c48f3b3b7859dcdcd5b73e8918b156b2b2
Lucas de Freitas <lucasffm@pop-os.localdomain>
2020-01-13 19:36:39 -0300

    remove variables

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,4 @@
-// Target server hostname or IP address
-const TARGET_SERVER_HOST = process.env.TARGET_SERVER_HOST
-  ? process.env.TARGET_SERVER_HOST.trim()
-  : '';
-// Target server username
-const TARGET_SERVER_USER = process.env.TARGET_SERVER_USER
-  ? process.env.TARGET_SERVER_USER.trim()
-  : '';
-// Target server application path
-const TARGET_SERVER_APP_PATH = `/www/${process.env.APP_NAME}`;
-// Your repository
-const REPO = 'git@gitlab.com:lucasffm/nestjs-api-template.git';
-console.log('AQUIII');
-console.log(TARGET_SERVER_HOST);
-console.log(TARGET_SERVER_USER);
-console.log(TARGET_SERVER_APP_PATH);
-console.log(REPO);
-
 module.exports = {
-  /**
-   * Application configuration section
-   * http://pm2.keymetrics.io/docs/usage/application-declaration/
-   */
   apps: [
     {
       name: 'nestjs-sample',
@@ -44,15 +22,14 @@ module.exports = {
       },
     },
   ],
-
   deploy: {
     production: {
-      user: TARGET_SERVER_USER,
-      host: TARGET_SERVER_HOST,
+      user: 'ubuntu',
+      host: '134.209.170.246',
       ref: 'origin/master',
-      repo: REPO,
+      repo: 'git@gitlab.com:lucasffm/nestjs-api-template.git',
       ssh_options: 'StrictHostKeyChecking=no',
-      path: TARGET_SERVER_APP_PATH,
+      path: '/www/nestjs-api',
       'pre-deploy': 'rm yarn.lock || true &&  rm package-lock.json || true',
       'post-deploy':
         'env && node --version && npm config set scripts-prepend-node-path true && > .env && yarn run make:env && yarn install && yarn run build && yarn run migration:run && pm2 startOrRestart ecosystem.json --env production',
