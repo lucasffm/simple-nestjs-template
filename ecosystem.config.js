40bf9bb8461d5859b30c52202b8e03ad5c39ac2e
Lucas de Freitas <lucasffm@pop-os.localdomain>
2020-01-13 14:00:37 -0300

    added gitlab ci

---
+++ ecosystem.config.js
@@ -0,0 +1,60 @@
+// Target server hostname or IP address
+const TARGET_SERVER_HOST = process.env.TARGET_SERVER_HOST
+  ? process.env.TARGET_SERVER_HOST.trim()
+  : '';
+// Target server username
+const TARGET_SERVER_USER = process.env.TARGET_SERVER_USER
+  ? process.env.TARGET_SERVER_USER.trim()
+  : '';
+// Target server application path
+const TARGET_SERVER_APP_PATH = `/www/app`;
+// Your repository
+const REPO = 'git@gitlab.com:lucasffm/nestjs-api-template.git';
+
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    {
+      name: 'nestjs-sample',
+      script: 'dist/main.js',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 4545,
+        DB_HOST: localhost,
+        DB_PORT: 5432,
+        DB_USERNAME: 'postgres',
+        DB_PASSWORD: 'postgres',
+        DB_NAME: 'postgres',
+        JWT_SECRET: 'as7d9a8sd79as8d7as9d',
+        MAIL_USER: '12f88fb18d4e38',
+        MAIL_PASS: 'cf27a88d3022f9',
+        MAIL_HOST: smtp.mailtrap.io,
+      },
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    production: {
+      user: TARGET_SERVER_USER,
+      host: TARGET_SERVER_HOST,
+      ref: 'origin/master',
+      repo: REPO,
+      ssh_options: 'StrictHostKeyChecking=no',
+      path: TARGET_SERVER_APP_PATH,
+      'pre-deploy': 'rm yarn.lock || true &&  rm package-lock.json || true',
+      // Commands to be executed on the server after the repo has been cloned
+      'post-deploy':
+        'env && node --version && npm config set scripts-prepend-node-path true && > .env && yarn run make:env && yarn install && yarn run build && yarn run migration:run && pm2 startOrRestart ecosystem.json --env production',
+    },
+  },
+};
